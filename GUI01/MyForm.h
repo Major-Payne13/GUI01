d4caada1b9c3ae5d3e5393b8471569c4e13b3783
unknown <paynebri004@779CTE220LAB03.alpine.local>
2015-10-20 10:33:34 -0600

    had some fun practice with GUI

--- GUI01/MyForm.h
+++ GUI01/MyForm.h
@@ -15,6 +15,7 @@ namespace GUI01 {
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
 	public:
+		int times = 0;
 		MyForm(void)
 		{
 			InitializeComponent();
@@ -34,6 +35,14 @@ namespace GUI01 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^  button1;
+	protected:
+	private: System::Windows::Forms::TextBox^  textBox1;
+
+
+
+
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +57,52 @@ namespace GUI01 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(13, 13);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(184, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Press ONCE for a surprize!";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(13, 43);
+			this->textBox1->Multiline = true;
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(305, 134);
+			this->textBox1->TabIndex = 1;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(352, 346);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
-#pragma endregion
+	
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		times++;
+		if (times == 1)
+			textBox1->Text = textBox1->Text + "";
+		else
+			textBox1->Text = textBox1->Text + "I said to press ONCE for a surprize, idiot. ";
+		/*textBox1->Text = textBox1->Text + "Hello";*/
+	}
+	private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+	}
 	};
 }
