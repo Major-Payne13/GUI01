ea3c53dffdb5bccd637ac589dbe963b28bbf2335
unknown <paynebri004@779CTE220LAB03.alpine.local>
2015-10-22 10:32:36 -0600

    Had some fun with making a story

--- GUI01/MyForm.h
+++ GUI01/MyForm.h
@@ -39,6 +39,8 @@ namespace GUI01 {
 	protected:
 	private: System::Windows::Forms::TextBox^  textBox1;
 
+	private: System::Windows::Forms::ComboBox^  comboBox1;
+
 
 
 
@@ -59,32 +61,48 @@ namespace GUI01 {
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(13, 13);
+			this->button1->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->button1->Location = System::Drawing::Point(77, 207);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(184, 23);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"Press ONCE for a surprize!";
+			this->button1->Text = L"Countinue";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(13, 43);
+			this->textBox1->Location = System::Drawing::Point(12, 12);
 			this->textBox1->Multiline = true;
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(305, 134);
+			this->textBox1->Size = System::Drawing::Size(310, 139);
 			this->textBox1->TabIndex = 1;
+			this->textBox1->Text = L"On your walk through the forrest, you encounter a mama bear protecting her cubs. "
+				L" She looks quite hostile.  You have a dull sward with you.\r\n\r\nWHAT DO YOU DO\? \r\n"
+				L"";
 			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(2) { L"RUN!!!", L"Fight" });
+			this->comboBox1->Location = System::Drawing::Point(105, 157);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(121, 21);
+			this->comboBox1->TabIndex = 3;
+			this->comboBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &MyForm::comboBox1_SelectedIndexChanged);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(352, 346);
+			this->Controls->Add(this->comboBox1);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->button1);
 			this->Name = L"MyForm";
@@ -95,14 +113,49 @@ namespace GUI01 {
 		}
 	
 	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		times = 0;
 		times++;
-		if (times == 1)
-			textBox1->Text = textBox1->Text + "";
-		else
-			textBox1->Text = textBox1->Text + "I said to press ONCE for a surprize, idiot. ";
-		/*textBox1->Text = textBox1->Text + "Hello";*/
+		if (comboBox1->SelectedItem == L"Fight")
+	{
+		textBox1->Text = "You died.";
+	}
+	else
+		textBox1->Text = "You got away!";
+
+		/*
+		
+		if (times % 2 == 1 && times != 1)
+		{
+			textBox1->Text = "";
+		}
+		times++;
+			textBox1->Text =  "A button appeared!";
+
+			button2->Visible = true;
+			button1->Visible = false;*/
+		
 	}
 	private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+		
+		
 	}
-	};
+	
+	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+		
+	/*	times++;
+		if (times % 2 == 0)
+		{
+			textBox1->Text = "";
+		}
+		
+			textBox1->Text = "A button appeared!";
+			button1->Visible = true;
+			button2->Visible = false;
+		*/
+		
+	}
+private: System::Void comboBox1_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+	
+}	
+};
 }
